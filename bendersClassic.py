344dbaa0b9da9d82387055b94d323c976ef5df22
Roxanne (Thao) Nguyen <166986857+roxannen9n@users.noreply.github.com>
2026-09-15 11:19:08 +0700

    Update bendersClassic.py

--- bendersClassic.py
+++ bendersClassic.py
@@ -49,38 +49,38 @@ C, F, p, f = generateFacilityLocationData(C,F)
     
 
 
-def solveModelGurobi():
-    m2 = Model()
-    x = {j: m2.addVar(lb=0, vtype=GRB.BINARY) for j in range(F)}
+def solveModelGurobi(): #solve UFL directly Gurobi first. Model has both decision variables x and y
+    m2 = Model() #create a new Gurobi model
+    x = {j: m2.addVar(lb=0, vtype=GRB.BINARY) for j in range(F)} # create binary variables xj
     y = {(i, j): m2.addVar(lb=0, vtype=GRB.BINARY) for i in range(C) for j in range(F)}
-    for i in range(C):
+    for i in range(C): #each customer needs to be assigned to a facility
         m2.addConstr(sum([y[i, j] for j in range(F)]) == 1)
-    for j in range(F):
-        for i in range(C):
+    for j in range(F): # linking constraints btw opening variables and assignment variables
+        for i in range(C): # cant assign customers into unopened facility
             m2.addConstr(y[i, j] <= bigM*x[j])
     obj = 0
     for j in range(F):
-        obj = obj -f[j] * x[j]
+        obj = obj -f[j] * x[j] # calculate cost
         for i in range(C):
-            obj += p[i, j] * y[i, j]
-    m2.setObjective(obj, sense=GRB.MAXIMIZE)
-    m2.update()
+            obj += p[i, j] * y[i, j] # calculate revenue
+    m2.setObjective(obj, sense=GRB.MAXIMIZE) # input the obj into the gurobi
+    m2.update() 
     m2.Params.OutputFlag = 0
     m2.setParam('OutputFlag', False)
-    m2.optimize()
+    m2.optimize() #solve
     xVal= [x[j].x for j in range(F)]
     yVal =[y[i, j].x for i in range(C) for j in range(F)]
     return m2.objVal, xVal, yVal
 
 
-def subProblem(x):
+def subProblem(x): #submodel only optimizes y variable
     m1 = Model()
-    y = {(i, j):  m1.addVar(lb=0, vtype=GRB.CONTINUOUS) for i in range(C) for j in range(F)}
-    constrMu = {}
+    y = {(i, j):  m1.addVar(lb=0, vtype=GRB.CONTINUOUS) for i in range(C) for j in range(F)} #create yij variable
+    constrMu = {} # 2 dicts to save constraint object
     constrNu = {}
-    for i in range(C):
+    for i in range(C): # each customer needs to be served fully by sum of assignments from facilities
         constrMu[i] = m1.addConstr(sum([y[i, j] for j in range(F)]) == 1)
-    for j in range(F):
+    for j in range(F): # linking constraints 
         for i in range(C):
             constrNu[i, j] = m1.addConstr(y[i, j] <= bigM*x[j])
 
@@ -95,16 +95,17 @@ def subProblem(x):
 
     m1.optimize()
 
-    if m1.status == GRB.OPTIMAL:
-        mu = {}
+    if m1.status == GRB.OPTIMAL: #if sub-model feasible and optimal:
+        mu = {} #create dual variables
         nu = {}
         for i in range(C):
-            mu[i] = constrMu[i].pi
+            mu[i] = constrMu[i].pi #pi is shadow price of constraint 1 sum yij = 1
         for j in range(F):
             for i in range(C):
-                nu[i, j] = constrNu[i, j].pi
+                nu[i, j] = constrNu[i, j].pi # dual multiplier of constraint 2
         return m1.objVal, mu, nu, [y[i, j].x for i in range(C) for j in range(F)], m1.status
-    else:
+        #return subproblem obj val, muy, nuy, primal yij, solver status
+    else: #if infeasible then use faarkas dual to provide info to build feasibility cut for master problem
         mu = {}
         nu = {}
         for i in range(C):
@@ -118,7 +119,7 @@ def subProblem(x):
 
 def setupMasterProblemModel():
     m = Model()
-    eta =  m.addVar(vtype=GRB.CONTINUOUS, name ='eta')
+    eta =  m.addVar(vtype=GRB.CONTINUOUS, name ='eta') # create a placeholder for the value of the subproblem
     x = {j: m.addVar(lb=0, vtype=GRB.BINARY, name = str(j)) for j in range(F)}
     
     m.setObjective(eta -sum([f[j] * x[j] for j in range(F)]), sense=GRB.MAXIMIZE)
@@ -133,7 +134,7 @@ def solveMaster(m,  optCuts_mu, optCuts_nu, fesCuts_mu, fesCuts_nu):
     Adding optimality cut
     '''
     
-    if len(optCuts_nu) != 0:            
+    if len(optCuts_nu) != 0: #check feasible dual solution            
         tot = sum(optCuts_mu.values())    
         for j in range(F):
             for i in range(C):
@@ -143,12 +144,12 @@ def solveMaster(m,  optCuts_mu, optCuts_nu, fesCuts_mu, fesCuts_nu):
     '''
     Adding feasibility cut
     '''
-    if len(fesCuts_mu) != 0:            
+    if len(fesCuts_mu) != 0: # only run if subproblem infeasible           
         tot = sum(fesCuts_mu.values())
         for j in range(F):
             for i in range(C):
                 tot += fesCuts_nu[i, j] * m.getVarByName(str(j))* bigM
-        m.addConstr (tot >= 0)
+        m.addConstr (tot >= 0) #eliminate master decision x leading to subproblem infeasible
     
 
     
@@ -162,7 +163,7 @@ def solveMaster(m,  optCuts_mu, optCuts_nu, fesCuts_mu, fesCuts_nu):
 
 
 
-def solveUFLBenders(eps, x_initial, maxit, verbose=0):
+def solveUFLBenders(eps, x_initial, maxit, verbose=0): #run full benders decomposition in loop
     UB = float("inf")
     LB = -float("inf")
     optCuts_mu = []
@@ -207,7 +208,7 @@ def solveUFLBenders(eps, x_initial, maxit, verbose=0):
     return x, y, UB
 
 
-def checkGurobiBendersSimilarity(xb, yb, xg, yg):
+def checkGurobiBendersSimilarity(xb, yb, xg, yg): #compare benders results and full MILP results
 
     ind = 0
     for j in range(F):
